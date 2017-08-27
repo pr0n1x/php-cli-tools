c4c3798fe4221baa8c851bd9126a635affd6c1a1
pr0n1x <pr0n1x@yandex.ru>
2017-08-27 14:28:02 +0700

    Скрипт снятия бэкапа с битрикс: Добавлена новая фича. Автоматическое создание конфига: опция --make-config

--- bx_backup.sh
+++ bx_backup.sh
@@ -4,36 +4,44 @@
 # then config file must me named as "bx_backup.domain.ru.conf"
 # config file example:
 #
+#  backup_name="domain.ru"
 #  document_root="${HOME}/www/domain.ru";
 #  backup_folder="${HOME}/backup"
-#  backup_name="domain.ru"
 #
 
 self_dir=$(cd $(dirname $0); pwd;);
 self_name=`basename $0`;
 self_name=${self_name%.*};
+self_real_path=`realpath $0`;
+self_real_name=`basename $self_real_path`;
+self_real_name=${self_real_name%.*};
 current_time=`date +%Y-%m-%d-%H%M`;
 CWD=`pwd`;
 
 show_help() {
-	echo "Usage: [-h|-f|-p|...]"
-	echo "    -h | --help        - Show this help message"
-	echo "    -p | --pipe        - Do not create file and push data to stdout"
-	echo "    -f | --files       - Create backup of the bitrix program files"
-	echo "    -u | --upload      - Create backup of the bitrix upload folder"
-	echo "    -a | --all-files   - Create full backup except database dump file :)"
-	echo "    -w | --whole       - Not implemented yet. Create whole files to backup with database"
-	echo "    -d | --db          - Backup database"
-	echo "    -z | --gzip        - Use gzip compression"
-	echo "    -j | --bzip        - Use bzip2 compression"
-	echo "    -v | --tar-verbose - Show tar report (tar option -v)"
-	echo "    --tar-perm         - Save files permisions (tar option -p)"
-	echo "    --show-db-name     - mmm.. this option shows database name..."
-	echo "    --show-db-user     - mmm.. you know"
-	echo "    --show-db-pass     - Ah! Don't use this option in public places!"
-	echo "    --show-db-charset  - Ok. This option you can use any where."
-	echo "    --make-config      - Create config file for domain (virtual host)"
-	echo "                         This options works only if it's alone"
+	echo "Usage: ${self_name} [options]";
+	echo "    -h | --help        - Show this help message";
+	echo "    -p | --pipe        - Do not create file and push data to stdout";
+	echo "    -f | --files       - Create backup of the bitrix program files";
+	echo "    -u | --upload      - Create backup of the bitrix upload folder";
+	echo "    -a | --all-files   - Create full backup except database dump file :)";
+	echo "    -w | --whole       - Not implemented yet. Create whole files to backup with database";
+	echo "    -d | --db          - Backup database";
+	echo "    -z | --gzip        - Use gzip compression";
+	echo "    -j | --bzip        - Use bzip2 compression";
+	echo "    -v | --tar-verbose - Show tar report (tar option -v)";
+	echo "    --tar-perm         - Save files permisions (tar option -p)";
+	echo "    --show-db-name     - mmm.. this option shows database name...";
+	echo "    --show-db-user     - mmm.. you know";
+	echo "    --show-db-pass     - Ah! Don't use this option in public places!";
+	echo "    --show-db-charset  - Ok. This option you can use any where.";
+	show_mk_conf_help;
+}
+show_mk_conf_help() {
+	echo "    --make-config      - Create config file for domain (virtual host) and backup script symlink";
+	echo "                         --make-config <domain-name> [document root path] [backup folder path]";
+	echo "                         Example:";
+	echo "                         --make-config domain.ru ~/ext_www/domain.ru ~/backup";
 }
 OPTS=`getopt -o hpfuadwzjv --long 'help,pipe,files,upload,all,db,whole,gzip,zip,bzip2,bzip,tar-verbose,tar-perm,show-db-name,show-db-user,show-db-pass,show-db-charset,make-config' -n 'parse-options' -- $@`
 #echo $OPTS;
@@ -130,15 +138,67 @@ while (( $# )); do
 		;;
 		--)
 			if [ "xY" = "x$make_config" ]; then
-				echo $0;
-				echo `realpath $0`;
-				echo "Making config";
+				echo "Making config...";
 				#echo "Opts: $@";
+				mk_conf_domain=`echo $2 | sed 's/[[:space:]]//g'`
+				mk_conf_domain=`echo $2 | sed 's/[[:space:]]//g'`
+				mk_conf_document_root=`echo $3 | sed 's/[[:space:]]//g' | sed "s#${HOME}#\\\${HOME\}#g"`;
+				mk_conf_backup_folder=`echo $4 | sed 's/[[:space:]]//g' | sed "s#${HOME}#\\\${HOME\}#g"`;
+				if [ "x" = "x$mk_conf_domain" ]; then
+					echo "Error: You should to set domain name" 1>&2;
+					show_mk_conf_help 1>&2;
+					exit 1;
+					#mk_conf_domain="domain.ru";
+				fi
+				if [ "x" = "x$mk_conf_document_root" ]; then
+					mk_conf_document_root="\${HOME}/ext_www/${mk_conf_domain}"
+				fi
+				if [ "x" = "x$mk_conf_backup_folder" ]; then
+					mk_conf_backup_folder="\${HOME}/backup"
+				fi
+				mk_conf_new_self=${self_dir}/${self_real_name}.${mk_conf_domain}.sh;
+				mk_conf_new_conf=${self_dir}/${self_real_name}.${mk_conf_domain}.conf;
+				echo "mk_conf_domain=${mk_conf_domain}";
+				echo "mk_conf_document_root=${mk_conf_document_root}";
+				echo "mk_conf_backup_folder=${mk_conf_backup_folder}";
+				echo "mk_conf_new_self=${mk_conf_new_self}";
+				echo "mk_conf_new_conf=${mk_conf_new_conf}";
+				
+				if [ -f ${mk_conf_new_self} ]; then
+					echo "Warning: new domain backup script symlink already exists." 1>&2;
+					if [ ! -L ${mk_conf_new_self} ]; then
+						echo "         but... this file is not a symlink to real backup script!"
+						echo "         You should delete it and run --make-config again."
+					fi
+				else
+					ln -s ${self_real_path} ${mk_conf_new_self};
+				fi
+				if [ -f ${mk_conf_new_conf} ]; then
+					echo "Warning: new domain config already exists." 1>&2;
+				else
+					cat <<EOF > ${mk_conf_new_conf};
+backup_name="${mk_conf_domain}";
+document_root="${mk_conf_document_root}";
+backup_folder="${mk_conf_backup_folder}";
+
+EOF
+					mk_conf_editor=`which mcedit`;
+					if [ "x" = "x${mk_conf_editor}" ]; then
+						mk_conf_editor=`which nano`;
+					fi
+					if [ "x" = "x${mk_conf_editor}" ]; then
+						mk_conf_editor=`which vim`;
+					fi
+					if [ "x" != "x${mk_conf_editor}" ]; then
+						$mk_conf_editor $mk_conf_new_conf;
+					fi
+				fi
 				exit 0;
 			fi
 			
+			# check config
 			if [ ! -f ${self_dir}/${self_name}.conf ]; then
-				echo "Config file ${self_name}.conf not found";
+				echo "Error: Config file \"${self_name}.conf\" not found" 1>&2;
 				exit 1;
 			fi
 
@@ -158,6 +218,7 @@ while (( $# )); do
 				exit 1;
 			fi
 
+			# read bitrix config
 			backup_filename="${backup_name}.bak-${current_time}"
 			backup_filepath="$backup_folder/$backup_filename"
 			db_user=`cat $document_root/bitrix/php_interface/dbconn.php | egrep -v '^[[:space:]]*(//|#)' | grep '\$DB' | grep '\$DBLogin' | awk -F '=' '{print $2}' | awk -F '"' '{print $2}'`;
@@ -169,7 +230,7 @@ while (( $# )); do
 				db_default_charset=utf8
 			fi
 			
-			
+			# check errors
 			return_status=0;
 			notPipeFileAndDb="Error: You can't make backup of files and database and use pushing to pipe at the same time";
 			if [ "xY" = "x$use_pipe" ] && [ "xY" = "x$make_db" ] && [ "xY" = "x$make_files" ]; then
